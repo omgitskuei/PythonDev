5f1213a52872bd279443f14836e7cf2557245bbf
omgitskuei <kueifengtungchris@gmail.com>
2020-06-11 10:27:59 +0800

    June 11

---
+++ references/basics/comments.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+Comments
+Python has commenting capability for the purpose of in-code documentation.
+
+Comments start with a #, and Python will render the rest of the line as a comment:
+
+Example
---
+++ references/basics/variables.py
@@ -0,0 +1,10 @@
+Python Variables
+In Python, variables are created when you assign a value to it:
+
+Example
+Variables in Python:
+
+x = 5
+y = "Hello, World!"
+
+Python has no command for declaring a variable
