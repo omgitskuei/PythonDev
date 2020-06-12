3c5405afe9040b24ca43bd11e1fc5742b0ca8e63
omgitskuei <kueifengtungchris@gmail.com>
2020-06-12 15:01:11 +0800

    June 12 - added set.pop()

---
+++ references/dataTypes/sets/set.py
@@ -0,0 +1,8 @@
+aSet = set(("apple", "tuna", "sandwich"))
+# aSet[0] = "berries" - would cause error
+# print(aSet+aSet) set does not support + or * operants
+
+# Removes the first one from set
+# eg. removes 'apple' first, then 'tuna', lastly removes 'sandwich' )
+aSet.pop()
+# if you pop() an empty set, error KeyError: 'pop from an empty set'
