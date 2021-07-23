c99fdddb74a45e310b3919991678d0ecfa7d918c
Kuei <57666754+omgitskuei@users.noreply.github.com>
2021-07-23 17:48:15 +0800

    7-23 list comp

--- references/dataTypes/sequences/list/listComprehension.py
+++ references/dataTypes/sequences/list/listComprehension.py
@@ -5,53 +5,137 @@ List comprehension offers a shorter syntax when you want to create a new list
 based on the values of (an) existing list(s).
 
 Syntax
+newlist = [EXPRESSION for ITEM in ITERABLE if CONDITION]
+- The 'CONDITION' is a filter that only accepts items that valuate to True.
+- The 'ITERABLE' can be any iterable object, like a list, tuple, set etc.
+- The 'EXPRESSION' is the current item in the iteration, but it is also the
+  outcome, which you can manipulate before it ends up like a list item in the
+  new list.
 A list comprehension consists of brackets '[]' containing an expression
 followed by a for clause, then zero or more for or if clauses.
 
-newlist = [expression for item in iterable if condition == True]
-- The 'condition' is like a filter that only accepts the items that valuate to
-  True.
-- The 'iterable' can be any iterable object, like a list, tuple, set etc.
-- The 'expression' is the current item in the iteration, but it is also the
-  outcome, which you can manipulate before it ends up like a list item in the
-  new list.
+@author: omgitskuei
 """
+# In[0]
+print('Return a new list that is the SAME as the iterable')
+things = [1, 2.2, "a"]
+newList = []
+for item in things:
+    newList.append(item)
+print(f'{newList=}')    # prints newList=[1, 2.2, 'a']
 
-# Set all values in the new list to 'hello'
-fruits = ["apple", "banana"]
-newList1 = []
-for eachStr in fruits:
-    newList1.append('hello')
-print(f'{newList1=}')      # prints ['hello', 'hello']
 # list comprehension equivalent
-compList1 = ['hello' for x in fruits]
-print(f'{compList1=}')     # prints ['hello', 'hello']
+newList = [item for item in things]    # no CONDITION after ITERBLE
+print(f'{newList=}')
 
-# Return "orange" instead of "banana":
+
+# In[1]
+print('Return a new list with 1 condition')
+numbers = [1, 2.2, 4, 6, 10]
+newList = []
+for number in numbers:
+    if number < 5:
+        newList.append(number)
+print(f'{newList=}')    # prints newList=[1, 2.2, 4]
+
+# list comprehension equivalent
+newList = [number for number in numbers if number < 5]
+print(f'{newList=}')    # prints newList=[1, 2.2, '4']
+
+# Only accept items that are not "apple":
+fruits = ["apple", "banana", "cherry"]
+newList = []
+for fruit in fruits:
+    if fruit != 'apple':
+        newList.append(fruit)
+print(f'{newList=}')    # prints ["banana", "cherry"]
+
+# list comprehension equivalent
+newList = [fruit for fruit in fruits if fruit != 'apple']
+print(f'{newList=}')    # prints ["banana", "cherry"]
+
+
+# In[2]
+print('Return a new list with 2 conditions')
+numbers = [1, 2.2, 4, 6, 10]
+newList = []
+for number in numbers:
+    if number < 5 and number > 1:
+        newList.append(number)
+print(f'{newList=}')    # prints newList=[2.2, 4]
+
+# list comprehension equivalent
+newList = [number for number in numbers if number < 5 and number > 1]
+print(f'{newList=}')    # prints newList=[2.2, '4']
+
+
+# In[3]
+print('Return a new list with overwritten Static values')
+fruits = ["apple", "banana"]
+newList = []
+for eachStr in fruits:
+    newList.append('hello')    # Set all values in the new list to "hello"
+print(f'{newList=}')      # prints ['hello', 'hello']
+
+# list comprehension equivalent
+compList = ['hello' for each in fruits]
+print(f'{compList=}')     # prints ['hello', 'hello']
+
+
+# In[4]
+print('Return a new list where a specific value is overwritten')
+fruits = ["apple", "banana", "mango", "pear"]
+newList = []
+for eachFruit in fruits:
+    # ternary
+    newList.append('Bleh' if eachFruit == "mango" else eachFruit)
+print(f'{newList=}')      # prints newList=['apple', 'banana', 'Bleh', 'pear']
+
+# list comprehension equivalent
+compList = [fruit if fruit != "mango" else 'Bleh' for fruit in fruits]
+print(f'{compList=}')     # prints ['apple', 'banana', 'Bleh', 'pear']
+
+
+# In[5]
+print('Return "orange" instead of "banana":')
+newList = []
+fruits = ["apple", "banana", "cherry", "banana"]
+for eachStr in fruits:
+    if eachStr == "banana":
+        newList.append("orange")
+    else:
+        newList.append(eachStr)
+print(f'{newList=}')      # prints ['apple', 'orange', 'cherry', 'orange']
+
+# list comprehension equivalent
+compList = [x if x != "banana" else "orange" for x in fruits]
+print(f'{compList=}')     # prints ['apple', 'orange', 'cherry', 'orange']
+
+
+# In[6]
+
+
+
+
+
+
+
+
+
+
+# In[2]: Set the values in the new list to upper case:
 newList2 = []
 fruits = ["apple", "banana", "cherry"]
 for eachStr in fruits:
-    if eachStr == "banana":
-        newList2.append("orange")
-    else:
-        newList2.append(eachStr)
-print(f'{newList2=}')      # prints ['apple', 'orange', 'cherry']
+    newList2.append(eachStr.upper())
+print(f'{newList2=}')       # prints ['APPLE', 'BANANA', 'CHERRY']
 # list comprehension equivalent
-compList2 = [x if x != "banana" else "orange" for x in fruits]
-print(f'{compList2=}')     # prints ['apple', 'orange', 'cherry']
+compList2 = [x.upper() for x in fruits]
+print(f'{compList2=}')      # prints ['APPLE', 'BANANA', 'CHERRY']
 
-# Set the values in the new list to upper case:
-newList3 = []
-fruits = ["apple", "banana", "cherry"]
-for eachStr in fruits:
-    newList3.append(eachStr.upper())
-print(f'{newList3=}')       # prints ['APPLE', 'BANANA', 'CHERRY']
-# list comprehension equivalent
-compList3 = [x.upper() for x in fruits]
-print(f'{compList3=}')      # prints ['APPLE', 'BANANA', 'CHERRY']
 
-Accept only numbers lower than 5:
-newlist = [x for x in range(10) if x < 5]
+
+
 
 You can use the range() function to create an iterable:
 newlist = [x for x in range(10)]
@@ -59,8 +143,7 @@ newlist = [x for x in range(10)]
 With no if statement:
 newlist = [x for x in fruits]
 
-Only accept items that are not "apple":
-newlist = [x for x in fruits if x != "apple"]
+
 
 
 
