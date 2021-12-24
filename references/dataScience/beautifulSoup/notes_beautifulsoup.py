2e8aab3a4df3e4b304e5403a82a6db1ae2267fe4
omgitskuei <kueifengtungchris@gmail.com>
2021-12-25 00:14:01 +0800

    starting making notes from Web Scraping book

---
+++ references/dataScience/beautifulSoup/notes_beautifulsoup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Program name: notes_beautifulsoup
+Summary: My notes on BeautifulSoup, from
+'Web Scraping with Python, 2nd Edition, Ryan Mitchell, O'Reilly'
+Created on: Fri Dec 24 23:57:00 2021
+@author: omgitskuei (Github)
+"""
+
