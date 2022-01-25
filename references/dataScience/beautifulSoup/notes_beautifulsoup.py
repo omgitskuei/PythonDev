9dda0cf59dd82399e71d0bfad9a1e57c21358d58
omgitskuei <kueifengtungchris@gmail.com>
2022-01-25 21:19:38 +0800

    Delete notes_beautifulsoup.py

--- references/dataScience/beautifulSoup/notes_beautifulsoup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Program name: notes_beautifulsoup
-Summary: My notes on BeautifulSoup, from
-'Web Scraping with Python, 2nd Edition, Ryan Mitchell, O'Reilly'
-Created on: Fri Dec 24 23:57:00 2021
-@author: omgitskuei (Github)
-"""
-
