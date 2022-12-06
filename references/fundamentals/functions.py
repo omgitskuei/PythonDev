658a6c88ad44e00eaecd9173f76b7b6b8115c77f
Kuei <57666754+omgitskuei@users.noreply.github.com>
2022-12-06 10:59:20 +0800

    Update functions.py

--- references/fundamentals/functions.py
+++ references/fundamentals/functions.py
@@ -15,6 +15,7 @@ try:
     hello2(1)
 except TypeError:
     print('TypeError: can only concatenate str (not "int") to str')  # this will be printed
+hello1(name='Bob')
 
 
 def add2(num1: int, num2: int) -> int:
@@ -24,3 +25,6 @@ def add2(num1: int, num2: int) -> int:
 print(add2(1, 3))
 print(add2(1.234, 3.5))  # despite add2 specifying that it takes 2 ints, passing float works
 # though at least the IDE will point out that int was expected, not float, as warnings
+
+# what if you want to specify which number is num1 and num2?
+print(add2(num2=3, num1=1))  # same as above, verbose but explicit (easier to read, honestly)
